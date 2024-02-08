c2e3c7d338ff5039d0139eab9ca2329230690cf6
Anderson <andyvini.developer@gmail.com>
2024-02-08 21:46:50 +0100

    se ha garegado la función que al selecionar la fecha en el calendario se devuelva la fecha seleccionada mediante un alert, tambien se ha agregado al función al seleccionar una cita te devuelva los datos de la cita tambien lo mostramos en un alert ,el alert que usamos para mostras la fecha y los datos de la cita es provicional. Tambien se ha creado una interface para guardar los datos de la cita ya sea para mostrarlo o para enviarlo y guardarlo en la BD la interface se llama citas.ts

---
+++ src/app/interfaces/citas.ts
@@ -0,0 +1,5 @@
+export interface Citas {
+    hora:string;
+    nombre:string;
+    trabajo:string;
+}
--- src/app/pages/view-citas/view-citas.component.ts
+++ src/app/pages/view-citas/view-citas.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ComponentesPrimengModule } from '../../modulos/componentes-primeng-module/componentes-primeng.module';
-import { debug } from 'console';
+import { debug, log } from 'console';
+import { Observable } from 'rxjs';
+import { Citas } from '../../interfaces/citas';
+
 
 
 
@@ -14,11 +17,18 @@ import { debug } from 'console';
   styleUrl: './view-citas.component.css'
 })
 
-export class ViewCitasComponent {
-  date: Date | undefined;
-  horas: string[] = this.IntevaloHoras();
-  public IntevaloHoras(): string[]{
-    let arrayHoras:string[] = [];
+export class ViewCitasComponent{
+  dateCalendario:Date = new Date();
+  datecalendario2 = this.dateCalendario;
+  dateFormat:string | undefined;
+  datosCitas: Citas [] = [];
+
+
+  constructor(){
+    // console.log(this.dateFormat)
+    this.IntevaloHoras();
+  }
+  IntevaloHoras(){
     let HorasInicio:number = 0;
     let HorasFin:number = 23;
     let intervaloHorasMinutos:number = 15;
@@ -27,11 +37,24 @@ export class ViewCitasComponent {
       for ( let j = 0; j< 60; j += intervaloHorasMinutos){
         let hora= i.toString().padStart(2, "0");
         let minuto = j.toString().padStart(2, "0");
-        arrayHoras.push(`${hora}:${minuto}`);
+        // arrayHoras.push(`${hora}:${minuto}`);
+        this.datosCitas?.push({
+          hora: `${hora}:${minuto}`,
+          nombre: '',
+          trabajo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A vitae ab adipisci ipsam libero consequuntur voluptas. Neque suscipit exercitationem quo repellat incidunt odio nobis consectetur odit nihil aspernatur illum doloribus possimus, nam reiciendis qui nostrum itaque totam beatae porro architecto? Aut tempore tenetur doloremque ipsa error architecto neque doloribus fugit reiciendis, mollitia temporibus nulla quo necessitatibus. Iusto maiores similique dignissimos aut omnis quia veniam dolores, expedita in fugiat placeat porro voluptas. Reiciendis necessitatibus enim perferendis, fugiat nulla dignissimos odit maxime consequatur minus deleniti, quia magni nobis quisquam iusto, blanditiis minima facilis ea inventore. Harum quas officiis sunt autem dolores soluta velit deserunt nostrum dolor labore nisi quia ut sed, reiciendis consectetur adipisci molestiae doloribus explicabo placeat error ipsa. Possimus saepe quam, aspernatur nemo repellendus minus impedit minima ipsam adipisci quos iure. Quisquam magni autem sint non deleniti, ipsam dolorum neque est ullam quod recusandae exercitationem eius! Necessitatibus laudantium deserunt quam praesentium. Aliquid odio accusantium, iusto sint aspernatur tenetur laudantium error alias minima quaerat saepe consequatur impedit! Quibusdam voluptatum aliquid modi veritatis perferendis. Impedit maxime illum dolorum suscipit totam odio quos ex optio, necessitatibus eum voluptates repellendus, ratione iure, culpa provident laudantium recusandae animi enim ipsa fuga. Natus nam sit vitae.'
+        });
       }
     }
-    return arrayHoras;
+  }
+
+  selectDate(){
+    this.dateFormat = this.dateCalendario.toLocaleDateString();
+    alert("SE HA SELECCIONADO UNA FECHA"+ this.dateFormat);
   }
   
+  getDatoCita(dato:Citas){
+    let contenido = dato;
+    alert(`hora:${contenido.hora}\nNombre:${contenido.nombre}\nTrabajo:${contenido.trabajo}`);
+  }
 
 }
